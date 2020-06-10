019d26d1ac0a70734001978e9d299e0b6d94482a
Evan Bero <evanbero1980@gmail.com>
2020-06-10 17:15:09 -0400

    added style to all html and added images into assets folder

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// USER ACCOUNT PAGE 
-// javascript includes user input to set up user account (name, username, password,email,city and phone number)
-// city variable input by user will be used for weather api located on the dashboard
 
-var accountInfo=[];
 
+$("#slide-out").sideNav();
     // added the click event for the next button to procced to profile page
     $("#next1").on("click", function(event){
         event.preventDefault();
         window.location.href = "profile.html";
-        
-        
         });
         
         // added a click function for calulate button 
@@ -97,8 +92,6 @@ var accountInfo=[];
             // saving profile info to local storage
             profileInfo.push(age,startWeight,inches,endWeight,goal,dailyCalorieIntake)
             localStorage.setItem("profileInfo",JSON.stringify(profileInfo));
-        
-        
         });
 
 // end of profile javascript
@@ -115,7 +108,7 @@ var accountInfo=[];
         $("#date").text(date);
 
         var cityArray=[];
-        var weeklyCalender=[];
+        var weeklyCalendar=[];
         var currentWeight=[];
         retrieveCity();
         storeCity();
@@ -231,8 +224,6 @@ var accountInfo=[];
           var sun= $("#sun").val().trim();
           weeklyCalender.push(mon,tue,wed,thur,fri,sat,sun);
           localStorage.setItem("weeklyCalender",JSON.stringify(weeklyCalender));
-         
-
         });
 
         // local storage for update weight section below 
@@ -523,6 +514,80 @@ function addDailyWorkout(){
 //   WorkoutRoutineArray = JSON.parse(localStorage.getItem("WorkoutRoutineArray") || "[]")
 // }
 
+// grpah section of javascript page 
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
+var graphDetails = []
+// add local storage to get profile Array
+var profile = {startWeight: 200,
+goalWeight:180,
+timeFrame:60}
+var startWeight = profile.startWeight
+var goalWeight = profile.goalWeight
+var timeFrame = profile.timeFrame
+var lossDay = (startWeight - goalWeight)/timeFrame
+// add local storage to create weightJournal based on user inputted weights
+var weighthJournal = [{entryDay: 10,weight:195},
+{entryDay:20,weight:190},
+{entryDay:30,weight:185}]
+
+var weightEntryID = 0
+var userWeight = startWeight
+var dayInput = weighthJournal[0].entryDay
+
+// Creates array for Graph Details using User profile and weight journal
+for(i=0;i<timeFrame;i++){
+  console.log(dayInput)
+  
+  if(i === dayInput){
+    userWeight = weighthJournal[weightEntryID].weight
+    weightEntryID++
+    if(weightEntryID = weighthJournal.length){
+      weightEntryID = weighthJournal.length -1
+    }
+    dayInput = weighthJournal[weightEntryID].entryDay
+  }
+
+  // add momement function to change i to date
+  graphDetails.push([i,startWeight,userWeight])
+  startWeight = startWeight - lossDay
+}
+
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Goal');
+      data.addColumn('number', 'Actual');
+      data.ready
+           
+
+      console.log(graphDetails)
+      data.addRows(
+       graphDetails
+      );
+
+      var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Weight'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+
+  
+    // Or with jQuery
+  
+    
+    
 
 //   End Generate Workout javascript
 
